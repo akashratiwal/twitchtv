fb59f338d1f7fc8adf0ff0ffddb466fea0dc425f
akash_verma_ <akash.verma@okutech.in>
2016-01-30 20:33:06 +0530

    Design is poor

--- js/twitch.js
+++ js/twitch.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
   console.log("started");
   var users = ["esl_sc2", "ogamingsc2", "freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
-  for (var user in users) {
-    $.ajax({
-      url: 'https://api.twitch.tv/kraken/channels/' + users[user],
-      dataType: 'jsonp',
-      success: function (val) {
-        if (val.logo != null) {
-          if (val.status != null) {
-            var tag_add = "<div class='items'><img src='" + val.logo + "' class='display_pics'>" + val.display_name + "<div class='status'></div></div>";
-          } else {
-            var tag_add = "<div class='items' style='background: #E5E4D7;'><img src='" + val.logo + "' class='display_pics'>" + val.display_name + "<div class='status'></div></div>";
-          }
-        } else {
-          var no_image_path = "http://www.hotnewsweekly.com/sites/default/files/News/25_1135.jpg";
-          if (val.status != null) {
-            var tag_add = "<div class='items'><img src='" + no_image_path + "' class='display_pics'>" + val.display_name + "<div class='status'></div></div>";
-          } else {
-            var tag_add = "<div class='items' style='background: #E5E4D7;'><img src='" + no_image_path + "' class='display_pics'>" + val.display_name + "<div class='status'></div></div>";
-          }
-        }
-        //console.log(tag_add);
-        $("#container").append(tag_add);
+  users.forEach(function(user) {
+    var tag_add = "";
+    $.getJSON("https://api.twitch.tv/kraken/streams/" + user, function(json) {
+      if (json.stream == null) {
+        var is_online = false;
+      } else {
+        var is_online = true;
       }
+      function handle(is_on) {
+        return function(data) {
+          var tag_add = "";
+          if (is_on) {
+            tag_add = "<div class='items' style='background: #E5E4D7'><img src='";
+          } else {
+            tag_add = "<div class='items'><img src='";
+          }
+          if (data.logo != null) {
+            tag_add += data.logo;
+          } else {
+            tag_add += "http://www.hotnewsweekly.com/sites/default/files/News/25_1135.jpg";
+          }
+          if (is_on) {
+            tag_add += "' class='display_pics'/>" + data.display_name + "<div class='user_status'>" + data.status + "</div></div>";
+            $("#container").prepend(tag_add);
+          } else {
+            tag_add += "' class='display_pics'/>" + data.display_name + "<div class='user_status'>Offline</div></div>";
+            $("#container").append(tag_add);
+          }
+        };
+      }
+      $.getJSON(json._links.channel, handle(is_online));
     });
-  }
+  });
 });
